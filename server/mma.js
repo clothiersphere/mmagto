689acbf5bf5db9f5b20609e77263ba935884a68b
ChristopherLee <xchrislee@gmail.com>
2017-07-27 16:58:04 -0700

    Layout format to parse obj.

--- server/mma.js
+++ server/mma.js
@@ -19,3 +19,83 @@ function getOdds(req, res, next) {
 module.exports = {
   getOdds
 };
+
+
+
+
+//obj
+  //banner [ {}, {} ]
+  //game - 12 item []
+    // $": {
+    // "idgm": "2491575",
+    // "gdesc": "",
+    // "idgmtyp": "107",
+    // "gmdt": "20170729",
+    // "idlg": "206",
+    // "gmtm": "21:15:00",
+    // "idspt": "MU",
+    // "vpt": "",
+    // "hpt": "",
+    // "vnum": "24225",
+    // "hnum": "24226",
+    // "evtyp": "",
+    // "idgp": "2491575",
+    // "gpd": "Game",
+    // "vtm": "Daniel Cormier",
+    // "htm": "Jon Jones",
+    // "stats": "false",
+    // "propCount": "0",
+    // "descgmtyp": "5k USD*"
+    // },
+  //line: [{
+    // $": {
+    // "voddst": "205",
+    // "hoddst": "-255",
+    // "ovt": "-4.5",
+    // "ovoddst": "-180",
+    // "unt": "4.5",
+    // "unoddst": "140",
+    // "vsprdt": "",
+    // "vsprdoddst": "",
+    // "hsprdt": "",
+    // "hsprdoddst": "",
+    // "vspt": "",
+    // "vspoddst": "",
+    // "hspt": "",
+    // "hspoddst": "",
+    // "voddsh": "+205",
+    // "hoddsh": "-255",
+    // "vsprdh": "",
+    // "hsprdh": "",
+    // "ovh": "o4&frac12;-180",
+    // "unh": "u4&frac12;+140",
+    // "vsph": "",
+    // "hsph": "",
+    // "voddshr": "0",
+    // "vsprdhr": "0",
+    // "ovhr": "0",
+    // "vsphr": "0",
+    // "vsproddsnr": "",
+    // "hsproddsnr": "",
+    // "ovoddsnr": "",
+    // "unoddsnr": "",
+    // "voddsnr": "",
+    // "hoddsnr": "",
+    // "vspoddsnr": "",
+    // "hspoddsnr": "",
+    // "voddsnrh": "",
+    // "hoddsnrh": "",
+    // "vsprdnrh": "",
+    // "hsprdnrh": "",
+    // "ovnrh": "",
+    // "unnrh": "",
+    // "vspnrh": "",
+    // "hspnrh": "",
+    // "btot": "False",
+    // "bsprd": "False",
+    // "bml": "False",
+    // "haschild": "True",
+    // "related": "True",
+    // "EmptyGame": "False"
+    // }
+  // }]
