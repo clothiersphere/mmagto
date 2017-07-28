8ae01d5464abb24ee2406e0b064b3c7ecaae0a4c
ChristopherLee <xchrislee@gmail.com>
2017-07-28 15:26:51 -0700

    Presenter update to display event header

--- src/components/BettingPage/presenter.js
+++ src/components/BettingPage/presenter.js
@@ -1,16 +1,36 @@
 import React from 'react';
 
-// var parseString = require('xml2js').parseString;
-// var xml = 'lines.bookmaker.eu';
-// parseString(xml, function (err,result) {
-//   console.dir(result);
-// })
+function BettingPage(events) {
 
-function BettingPage(odds) {
-  console.log(odds, "ODDSPRESENT")
-  return (
-    <h1> Do you have what it takes? </h1>
-  )
+function pushbutton(e) {
+  alert('it work')
+  e.preventDefault()
+}
+  if (events.fights[0]) {
+    return (
+      <div className="eventBox"> 
+      {
+        events.fights.map((event,key) => {
+          console.log(event, "event")
+          return <div key={key}>
+          <div>
+            {event.banner[0].$.htm}
+            {event.banner[0].$.vtm}
+            {event.banner[1].$.htm}
+            {event.banner[1].$.vtm}
+          <button type="button" onClick={pushbutton}>
+          </button>
+          </div>
+          </div>
+        })
+        
+
+      }
+      </div>
+    )
+  } else {
+    return <div><h1>..loading</h1></div>
+  }  
 }
 
 export default BettingPage;
