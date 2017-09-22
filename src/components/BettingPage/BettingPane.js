07294bd7ef9f4d611349d0c0fe4af2ce808e406c
ChristopherLee <xchrislee@gmail.com>
2017-09-22 16:48:30 -0700

    Add betting stat box: over/under vis/home Odds

--- src/components/BettingPage/BettingPane.js
+++ src/components/BettingPage/BettingPane.js
@@ -2,120 +2,16 @@ import React from 'react';
 import axios from 'axios';
 
 function BettingPane({events={}}) {
-  function displayEvent(selectedEvent) {
-    return selectedEvent.fights.map((fight, key) => {
-
-      function convertOdds(number) {
-        if (number > 0) {
-          var udParse = number.substr(number.indexOf('+')+1)
-          return <div className="fighter odds"> $100 wins you ${udParse} </div>  
-        } else {
-          var favCalc = (number*(-1))/100
-          favCalc = Math.round(100/favCalc)
-          return <div className="fighter odds"> $100 wins you ${favCalc} </div>
-        }
-      }
-
-      console.log(fight, "fight");
-
-      function homeFighter() {
-        return (
-          <div className="homeFighter">
-            <div className="fighter portrait">
-              <img className="portrait" src={fight.homeInfo.profile_image}/>
-            </div>
-            <div className="fighter name">
-              {fight.homeInfo.first_name} {fight.homeInfo.last_name}
-              <br/>
-              {fight.homeInfo.wins}-{fight.homeInfo.losses}-{fight.homeInfo.draws}
-            </div>
-              <div className="fighter odds">
-                {convertOdds(fight.visitorOdds)}
-              </div>
-              <div className="fighter slider">
-                <form className="payoutSlider">
-                  <input type="slider"/>
-                </form>
-              </div>
-              <div className="fighter check">
-                <form className="winnerCheck">
-                  <input type="checkbox"/>
-                </form>
-              </div>
-            </div>
-          
-        )
-      }
-      function homeFighterNoInfo() {
-        return (
-          <div className="homeFighter">
-            <div className="fighter portrait">
-              <img className="portrait" src={fight.homeInfo.thumbnail}/>
-            </div>
-            <div className="fighter name">
-              {fight.home}
-            </div>
-            <div className="fighter odds">
-              {convertOdds(fight.homeOdds)}
-            </div>
-            <div className="fighter slider">
-              <form className="payoutSlider">
-                <input type="slider"/>
-              </form>
-            </div>
-            <div className="fighter check">
-              <form className="winnerCheck">
-                <input type="checkbox"/>
-              </form>
-            </div>
-          </div>
-        )
-      }
-      function visitorFighter() {
-        return (
-        <div className="visitorFighter">
-          <div className="fighter portrait">
-            <img className="portrait" src={fight.visitorInfo.profile_image}/>
-          </div>
-          <div className="fighter name">
-            {fight.visitorInfo.first_name} {fight.visitorInfo.last_name}
-            <br/>
-            {fight.visitorInfo.wins}-{fight.visitorInfo.losses}-{fight.visitorInfo.draws}
-          </div>
-          <div className="fighter odds">
-            {convertOdds(fight.visitorOdds)}
-          </div>
-          <div className="fighter slider">
-            <form className="payoutSlider">
-              <input type="slider"/>
-            </form>
-          </div>
-          <div className="fighter check">
-            <form className="winnerCheck">
-              <input type="checkbox"/>
-            </form>
-          </div>
-        </div>
-        )
-      }
-
-      return (
-        <div className="bettingPane" key={key}>
-        
-        </div>
-      )
-    })
-  }
-
+  
   function showRank(side) {
-    if ( side === visitor) {
+    if ( side === visitorInfo) {
       if (events.selectedFight.visitorInfo.rank === null) {
         return <div className="large_faceOff_visitorInfo_rank"> Rank: Unranked </div>
       } else {
         return <div className="large_faceOff_visitorInfo_rank"> Rank: #{ events.selectedFight.visitorInfo.rank } </div>
       }
     } else {
-      if ( side === home) {
+      if ( side === homeInfo) {
         if (events.selectedFight.homeInfo.rank === null) {
           return <div className="large_faceOff_homeInfo_rank"> Rank: Unranked </div>
         } else {
@@ -126,7 +22,7 @@ function BettingPane({events={}}) {
   }
 
   function showNickname(side) {
-    if ( side === visitor) {
+    if ( side === visitorInfo) {
       if (events.selectedFight.visitorInfo.nickname === null) {
         return (
           <div className="large_faceOff_visitorInfo_nickname">
@@ -136,12 +32,12 @@ function BettingPane({events={}}) {
       } else {
         return (
           <div className="large_faceOff_visitorInfo_nickname">
-            Nickname: {visitor.nickname}
+            Nickname: {visitorInfo.nickname}
           </div>
         )
       }
     } else {
-      if (side === home) {
+      if (side === homeInfo) {
         if (events.selectedFight.homeInfo.nickname === null) {
           return (
             <div className="large_faceOff_homeInfo_nickname">
@@ -151,7 +47,7 @@ function BettingPane({events={}}) {
         } else {
           return (
             <div className="large_faceOff_homeInfo_nickname">
-              Nickname: {home.nickname}
+              Nickname: {homeInfo.nickname}
             </div>
           )
         }
@@ -161,42 +57,57 @@ function BettingPane({events={}}) {
 
   if (events.selectedFight.homeInfo) {
 
-    var visitor = events.selectedFight.visitorInfo;
-    var home = events.selectedFight.homeInfo;
+    var matchInfo = events.selectedFight;
+    var visitorInfo = events.selectedFight.visitorInfo;
+    var homeInfo = events.selectedFight.homeInfo;
 
     return (
      <div className="large_faceOff_container">
       <div className="large_faceOff_visitor">
-      <img src={visitor.left_full_body_image} />
+      <img src={visitorInfo.left_full_body_image} />
       </div>
       <div className="large_faceOff_visitorInfo">
         <div className="large_faceOff_visitorInfo_name">
-          Name: {visitor.first_name} {visitor.last_name}
+          Name: {visitorInfo.first_name} {visitorInfo.last_name}
         </div>
-          {showNickname(visitor)}
+          {showNickname(visitorInfo)}
         <div className="large_faceOff_visitorInfo_weightclass">
-          Weightclass: {visitor.weight_class.replace(/\_/g, " ")}
+          Weightclass: {visitorInfo.weight_class.replace(/\_/g, " ")}
         </div>
-          {showRank(visitor)}
+          {showRank(visitorInfo)}
         <div className="large_faceOff_visitorInfo_record">
-          Record: {visitor.wins}-{visitor.losses}-{visitor.draws} (W-L-D)
+          Record: {visitorInfo.wins}-{visitorInfo.losses}-{visitorInfo.draws} (W-L-D)
+        </div>
+      </div>
+      <div className="betting_stats">
+        <div className="visitor_odds">
+          Visitor:{matchInfo.visitorOdds}
+        </div>
+        <div className="home_odds">
+          Home:{matchInfo.homeOdds}
+        </div>
+        <div className="over_odds">
+          Over {matchInfo.over.charAt(1)} Rd:{matchInfo.over.substr(matchInfo.over.indexOf(';')+1)}
+        </div>
+        <div className="under_odds">
+          Under {matchInfo.under.charAt(1)} Rd:{matchInfo.under.substr(matchInfo.under.indexOf(';')+1)}
         </div>
       </div>
       <div className="large_faceOff_homeInfo">
         <div className="large_faceOff_homeInfo_name">
-          Name: {home.first_name} {home.last_name}
+          Name: {homeInfo.first_name} {homeInfo.last_name}
         </div>
-        {showNickname(home)}
+        {showNickname(homeInfo)}
         <div className="large_faceOff_homeInfo_weightclass">
-          Weightclass: {home.weight_class.replace(/\_/g, " ")}
+          Weightclass: {homeInfo.weight_class.replace(/\_/g, " ")}
         </div>
-          {showRank(home)}
+          {showRank(homeInfo)}
         <div className="large_faceOff_homeInfo_record">
-          Record: {home.wins}-{home.losses}-{home.draws} (W-L-D)
+          Record: {homeInfo.wins}-{homeInfo.losses}-{homeInfo.draws} (W-L-D)
         </div>
       </div>
       <div className="large_faceOff_home">
-      <img src={home.right_full_body_image} />
+      <img src={homeInfo.right_full_body_image} />
       </div>
     </div>
     )
