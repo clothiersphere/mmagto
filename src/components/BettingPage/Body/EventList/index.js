aebdbd5ae6dbe62740d8a543433a2728fc6770c9
Christopher Lee <xchrislee@gmail.com>
2017-11-02 15:43:27 -0700

    Changed into functional component / linted

--- src/components/BettingPage/Body/EventList/index.js
+++ src/components/BettingPage/Body/EventList/index.js
@@ -1,33 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import EventItem from './EventItem';
 import EventListLoader from './EventListLoader';
 
-export default class EventList extends Component {
- render() {
-    const { fights, selectEvent } = this.props;
-
-    if (fights.length === 0) {
-      return <EventListLoader />;
-    } else {
-      return (
-        <ul className="eventList">
-          {fights.map(function(event, index) {
-            if (event.eventInfo) {
-              return (
-                <li
-                  className="eventItem"
-                  onClick={()=>selectEvent(event)}
-                  key={index} 
-                >
-                  <EventItem event={event} />
-                </li>
-              );
-            }
-            return null;
-          })}
-        </ul>
-      );
-    }
+export default function EventList({ fights, selectEvent }) {
+  if (fights.length === 0) {
+    return <EventListLoader />;
   }
+  return (
+    <ul className="eventList">
+      {fights.map((event, index) => {
+        if (event.eventInfo) {
+          return (
+            <li
+              className="eventItem"
+              onClick={() => selectEvent(event)}
+              key={index}
+            >
+              <EventItem event={event} />
+            </li>
+          );
+        }
+        return null;
+      })}
+    </ul>
+  );
 }
 
+EventList.propTypes = {
+  fights: PropTypes.arrayOf(PropTypes.object),
+  selectEvent: PropTypes.func.isRequired,
+};
+
+EventList.defaultProps = {
+  fights: [],
+};
