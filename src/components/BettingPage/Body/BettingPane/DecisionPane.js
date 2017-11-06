28eb8f3ff9d728a26525e7402cffbd240d5399f4
Christopher Lee <xchrislee@gmail.com>
2017-11-06 13:40:48 -0800

    Clean up logic for displaying decisionPane. Clean up function in WagerPane

--- src/components/BettingPage/Body/BettingPane/DecisionPane.js
+++ src/components/BettingPage/Body/BettingPane/DecisionPane.js
@@ -1,97 +1,82 @@
 import React from 'react';
 
-var convertOdds = (odds, wager) => {
-  if (isPositive(odds)) {
-    return calcFav(odds, wager);
-  } else {
-    return calcUnderdog(odds, wager);
-  }
-}
-
-var isPositive = (odds) => {
+function isPositive(odds) {
   if (odds > 0) {
     return false;
-  } else {
-    return true;
   }
-  return;
+  return true;
 }
 
-var isFavored = (selectedFight, selectedFighter) => {
-  const { homeInfo, visitorInfo } = selectedFight
-  const { odds } = selectedFighter; 
-  const smaller = Math.min(homeInfo.odds, visitorInfo.odds);
-
-  if (parseInt(odds) === parseInt(smaller)) {
-    return 'FAVORITE';
-  } else {
-    return 'UNDERDOG';
-  }
-}
-
-var calcUnderdog = (odds, wager) => {
+function calcUnderdog(odds, wager) {
   var odds = odds/100;
   return parseInt(wager * odds);
 }
 
-var calcFav = (odds, wager) => {
-  var odds = odds*(-1);
+function calcFav(odds, wager) {
+  var odds = odds * (-1);
   odds = 100/odds;
   return parseInt(wager * odds);
 }
 
-var oddsToDec = (odds) => {
-  var odds = odds;
-  
-  if (odds > 1) {
-    odds = odds/100;
-    
-  } else {
-    odds = odds * (-1)
-    odds = 100/odds
+function convertOdds(odds, wager) {
+  if (isPositive(odds)) {
+    return calcFav(odds, wager);
   }
+  return calcUnderdog(odds, wager);
+}
 
+function isFavored(selectedFight, selectedFighter) {
+  const { homeInfo, visitorInfo } = selectedFight;
+  const { odds } = selectedFighter;
+  const smaller = Math.min(homeInfo.odds, visitorInfo.odds);
+
+  if (parseInt(odds) === parseInt(smaller)) {
+    return 'FAVORITE';
+  }
+  return 'UNDERDOG';
+}
+
+function oddsToDec(odds) {
+  var odds = odds;
+  if (odds > 1) {
+    odds = odds / 100;
+  } else {
+    odds = odds * (-1);
+    odds = 100 / odds;
+  }
   return odds + 1;
 }
 
-var impliedProb = (odds) => {
-
-  console.log(odds)
+function impliedProb(odds) {
   var odds = odds;
-
   if (odds > 1) {
-    odds = 100/(parseInt(odds) + 100)
-    
-    
+    odds = 100/(parseInt(odds) + 100) 
   } else {
     odds = odds * (-1)
     odds = 100/odds
-    
   }
   return (odds * 100).toFixed(2)
 }
 
-var totalPayout = (odds, wagerValue) => {
-  
+function totalPayout(odds, wagerValue) { 
   var winMoney = convertOdds(odds, wagerValue);
   var wagerValue = parseInt(wagerValue);
   return winMoney + wagerValue;
 }
 
-var DecisionPane = (props) => {
-  const {selectedFighter, selectedFight, wagerValue} = props;
-
-  return (
-    <div className="decision_panel">
-      {selectedFighter.last_name} is currently a {selectedFighter.odds} {isFavored(selectedFight, selectedFighter)}.
-      <br/>
-      Implied odds to win are {impliedProb(selectedFighter.odds)}%.
-      <br/>
-      If {selectedFighter.last_name} wins, your bet of ${wagerValue} would
+export default function DecisionPane({ selectedFighter, selectedFight, wagerValue }) {
+  if (selectedFighter[0]) {
+    return (
+      <div className="decision_panel">
+        {selectedFighter.last_name} is currently a {selectedFighter.odds} {isFavored(selectedFight, selectedFighter)}.
         <br/>
-      win you ${convertOdds(selectedFighter.odds, wagerValue).toFixed(2)} for a total payout of ${totalPayout(selectedFighter.odds, wagerValue)}
-    </div>
-  )  
+        Implied odds to win are {impliedProb(selectedFighter.odds)}%.
+        <br/>
+        If {selectedFighter.last_name} wins, your bet of ${wagerValue} would
+        <br/>
+        win you ${convertOdds(selectedFighter.odds, wagerValue).toFixed(2)} for a total payout of ${totalPayout(selectedFighter.odds, wagerValue)}
+      </div>
+    );
+  }
+  return null;
 }
-
-export default DecisionPane;
--- src/components/BettingPage/Body/BettingPane/WagerPane.js
+++ src/components/BettingPage/Body/BettingPane/WagerPane.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Accordion, Icon, Form, Radio, Button, Step, Input, Label } from 'semantic-ui-react';
 import BettingSlider from './BettingSlider';
 import DecisionPane from './DecisionPane';
@@ -8,17 +9,17 @@ export default class WagerPane extends Component {
   state = { activeIndex: 0, wagerValue: 0 };
 
   handleChange = (e, { value }) => {
-  this.selectFighter(events, value)
-    this.setState({ value })
+    this.selectFighter(e, value);
+    this.setState({ value });
   }
 
-  selectFighter(events, value) {
-    events.selectFighter(value);
+  selectFighter(e, value) {
+    const { selectFighter } = this.props.other;
+    selectFighter(value);
   }
-  
+
   render() {
-    const { value } = this.state;
-    console.log(this.props, "WP PROPS")
+    const { wagerValue } = this.state;
 
     const {
       home,
@@ -33,8 +34,6 @@ export default class WagerPane extends Component {
       selectedFight,
       selectedFighter,
     } = this.props.other;
-
-    console.log(selectedFight, selectedFighter, "SF")
 
     return (
       <div className="wagerPane">
@@ -68,40 +67,16 @@ export default class WagerPane extends Component {
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
-        <DecisionPane
-          selectedFighter={selectedFighter}
-          selectedFight={selectedFight}
-          wagerValue={this.state.wagerValue}
-        />
+        <DecisionPane {...{ selectedFight, selectedFighter, wagerValue }} />
       </div>
     );
-    // return (
-    //   <div className="wagerPane">
-    //     <Accordion>
-    //       <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
-    //         <Icon name="dropdown" />
-    //         Select a fighter to wager on.
-    //       </Accordion.Title>
-    //       <Accordion.Content active={activeIndex === 0}>
-    //         
-    //       </Accordion.Content>
-          
-    //       <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
-    //         <Icon name='dropdown' />
-    //         Select amount to wager.
-    //       </Accordion.Title>
-    //       <Accordion.Content active={activeIndex === 1}>
-    //       {this.wagerValue}
-    //         <Form>
-    //           <Form.Group>
-    //             <Form.Input placeholder='Enter amount to wager' value={this.wagerValue} onChange={this.wagerChange} />
-    //             <Button type='submit'> Submit </Button> 
-    //           </Form.Group>
-    //         </Form>
-    //       </Accordion.Content>
-
-    //     </Accordion>
-    //   </div>
-    // );
   }
 }
+
+WagerPane.propTypes = {
+  selectedFighter: PropTypes.arrayOf(PropTypes.object),
+};
+
+WagerPane.defaultProps = {
+  selectedFighter: [{}],
+};
