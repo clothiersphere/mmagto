3c792f6c5c70fb5e1ced3bda18397c6f62fcdc4e
Christopher Lee <xchrislee@gmail.com>
2017-11-07 13:01:31 -0800

    Some linting.

--- src/actions/selectFight.js
+++ src/actions/selectFight.js
@@ -3,13 +3,13 @@ import * as actionTypes from '../constants/actionTypes';
 function setFight(fight) {
   return {
     type: actionTypes.FIGHT_SET,
-    fight
+    fight,
   };
-};
+}
 
 export function selectFight(fight) {
-  return function(dispatch) {
-    dispatch({type: 'FIGHTER_RESET'})
-    dispatch(setFight(fight))
+  return function (dispatch) {
+    dispatch({ type: 'FIGHTER_RESET' });
+    dispatch(setFight(fight));
   };
-};
+}
