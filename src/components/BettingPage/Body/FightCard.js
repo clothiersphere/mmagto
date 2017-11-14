ac0d29cc526312cfe684a339df5637a02a199299
Christopher Lee <xchrislee@gmail.com>
2017-11-13 16:14:18 -0800

    Added failsafe in case UFC didn't update their DB with newly added/last minute fighters.

--- src/components/BettingPage/Body/FightCard.js
+++ src/components/BettingPage/Body/FightCard.js
@@ -16,22 +16,28 @@ export default function FightCard({ selectedEvent, selectFight, selectedFight })
 
   return (
     <div className="fightCard">
-      {selectedEvent[0].fights.map((fight, key) => (
+      {selectedEvent[0].fights.map((fight, key) => {
+        if (!fight.homeInfo || !fight.visitorInfo) {
+          return null;
+        }
+        return (
         <div className="fightCardFaceOff" role="link" key={key} onClick={() => selectFight(fight)}>
           <div className="small_faceOff_home">
-            <img className="faceOff_home small_portrait" src={fight.homeInfo.profile_image}/>
+            <img className="faceOff_home small_portrait" src={fight.homeInfo.profile_image} />
             <div className="home home_faceOff_LastName">
               {convertName(fight.homeInfo.last_name)}
             </div>
           </div>
           <div className="small_faceOff_visitor">
-            <img className="faceOff_visitor small_portrait" src={fight.visitorInfo.profile_image}/>
+            <img className="faceOff_visitor small_portrait" src={fight.visitorInfo.profile_image} />
             <div className="visitor visitor_faceOff_LastName">
               {convertName(fight.visitorInfo.last_name)}
             </div>
           </div>
         </div>
-        ))}
+      )
+      }
+      )}
     </div>
   );
 }
