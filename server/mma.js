b9e14434b2038510cc278dddd4a83ca794c4b35d
Christopher Lee <xchrislee@gmail.com>
2017-11-16 16:51:34 -0500

    Going to redo sever w/ await/async.

--- server/mma.js
+++ server/mma.js
@@ -2,16 +2,12 @@ const axios = require('axios');
 const parseString = require('xml2js').parseString;
 const mma = require('mma');
 
-// function camelize(str) {
-//   return str.replace(/\W+(.)/g, ((match, chr) =>
-//     chr.toUpperCase()
-//   ));
-// }
-
-function getEvents(req, res, next) {
+async function getEvents(req, res, next) {
   const bookmakerAPI = 'http://lines.bookmaker.eu';
   const ufcEventsAPI = 'http://ufc-data-api.ufc.com/api/v3/iphone/events';
   const ufcFightersAPI = 'http://ufc-data-api.ufc.com/api/v3/iphone/fighters';
+
+  // const queryBookMaker = await axios.get(bookmakerAPi.then(response => )
 
   axios.get(bookmakerAPI).then(response => parseString(response.data, { explicitChildren: true, preserveChildrenOrder: true }, (err, result) => {
     // had to set options: explicitChildren, preserveChildrenOrder to get correct order
@@ -29,7 +25,6 @@ function getEvents(req, res, next) {
     data.push(result.Data.$$[0].$$.find(x => x.$.IdLeague === '206'))
     // non UFC feed
     // data.push(result.Data.Leagues[0].league.find( x => x.$.IdLeague === '12639'))
-    
     // console.log(data[0]['banner'][1]['$']['vtm'], "data")
     // parse data into seperate arrays
     var parsedData = fightParser(data[0].$$);
@@ -39,49 +34,49 @@ function getEvents(req, res, next) {
     axios.get(ufcFightersAPI).then(response => response.data)
       .then((fighterList) => {
         const hasNick = fighterList.filter(x => x.nickname);
-        for (let i = 0; i < parsedData.length; i += 1) {
-          for (let j = 0; j < parsedData[i]['fights'].length; j += 1) {
-            visitor = parsedData[i]['fights'][j]['visitor'].replace(/\"/g, '');
+        const searchFighterDB = (first, last) => {
+          let fighterData = fighterList.find(x => x.last_name === last && x.first_name === first);
+          if (!fighterData) {
+            fighterData = hasNick.find(x => x.nickname === first);
+            if (!fighterData) {
+              fighterData = fighterList.find(x => x.last_name === last);
+              if (!fighterData) {
+                fighterData = fighterList.find(x => x.last_name === first);
+              }
+            }
+          }
+          return fighterData;
+        };
+        const fighterParse = (name, side) => {
+          if (side === 'v') {
+            visitor = name.replace(/\"/g, '');
             visitorFirst = visitor.substr(0, visitor.indexOf(' '));
             visitorLast = visitor.substr(visitor.indexOf(' ') + 1);
-            visitorLast = visitorLast.substr(visitorLast.indexOf(' ')+1);
-            home = parsedData[i].fights[j].home.replace(/\"/g, '');
-            homeFirst = home.substr(0, home.indexOf(' '));
-            homeLast = home.substr(home.indexOf(' ') + 1);
-            homeLast = homeLast.substr(homeLast.indexOf(' ') + 1);
-
-            const searchFighterDB = (first, last) => {
-              let fighterData = fighterList.find(x => x.last_name === last && x.first_name === first);
-              if (!fighterData) {
-                fighterData = hasNick.find(x => x.nickname === first);
-                if (!fighterData) {
-                  fighterData = fighterList.find(x => x.last_name === last);
-                  if (!fighterData) {
-                    fighterData = fighterList.find(x => x.last_name === first);
-                  }
-                }
-              }
-              return fighterData;
-            };
-
+            visitorLast = visitorLast.substr(visitorLast.indexOf(' ') + 1);
+          }
+          home = name.replace(/"/g, '');
+          homeFirst = home.substr(0, home.indexOf(' '));
+          homeLast = home.substr(home.indexOf(' ') + 1);
+          homeLast = homeLast.substr(homeLast.indexOf(' ') + 1);
+        };
+        for (let i = 0; i < parsedData.length; i += 1) {
+          for (let j = 0; j < parsedData[i].fights.length; j += 1) {
+            fighterParse(parsedData[i].fights[j].visitor, 'v');
+            fighterParse(parsedData[i].fights[j].home, 'h');
             parsedData[i].fights[j].homeInfo = searchFighterDB(homeFirst, homeLast, hasNick);
             parsedData[i].fights[j].visitorInfo = searchFighterDB(visitorFirst, visitorLast, hasNick);
-
             
-            
-            // if (i === 0 && j === 0) {
-            //   var homeName = parsedData[i].fights[j].home;
-            //   mma.fighter(homeName, function(data) {
-            //     parsedData[i].fights[j].homeData = data;
-            //   })
-
-
-
-            // }
+            if (i === 0 && j === 0) {
+              const homeName = parsedData[i].fights[j].home;
+              mma.retrieve(homeName).then((data) => {
+                console.log(data, "data")
+                parsedData[0].fights[0].homeInfo = data;
+                
+              })
+            }
           }
         }
-      })
-        .then(() => {
+      }).then(() => {
           axios.get(ufcEventsAPI).then(response => response.data)
             .then((data) => {
               for (var i = 0; i < parsedData.length; i++) {
@@ -194,6 +189,8 @@ function parseFighterInfo(array) {
     }
   return result;
 }
+
+
 
 module.exports = {
   getEvents
