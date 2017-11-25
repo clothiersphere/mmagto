dddaf6dfca4c7eae899a525648e7e5bd41a26b20
Christopher Lee <xchrislee@gmail.com>
2017-11-25 13:53:49 -0800

    console.log cleanup

--- server.js
+++ server.js
@@ -2,22 +2,16 @@ const express = require('express');
 const mma = require('./server/mma');
 const app = express();
 
-const data = require('./tempData.js');
-
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/api/getEvents', mma.getEvents);
-// app.get('/api/getEvents', (req, res) => {
-//   res.send(data);
-// });
+
 app.get('/api/getFighterStats/:id', mma.getFighterStats);
 
 app.listen(1234, () => {
-  console.log('%s listening at %s', app.name, app.url);
+  console.log('%s listening at %d', app.name, 1234);
 });
 
-
 module.exports = 'api';
-
