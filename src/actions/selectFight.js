2b374bc32347d7057f661c265016597bfbb53aaa
Christopher Lee <xchrislee@gmail.com>
2017-11-24 17:37:11 -0800

    added get requests / processing of FighterStats

--- src/actions/selectFight.js
+++ src/actions/selectFight.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionTypes from '../constants/actionTypes';
 
 function setFight(fight) {
@@ -9,7 +10,38 @@ function setFight(fight) {
 
 export function selectFight(fight) {
   return function (dispatch) {
-    dispatch({ type: 'FIGHTER_RESET' });
-    dispatch(setFight(fight));
+    const { home, visitor } = fight;
+
+    function getHome() {
+      const request1 = axios({
+        method: 'GET',
+        url: 'http://localhost:8080/api/getFighterStats/' + home,
+      });
+
+      return request1;
+    }
+    function getVisitor() {
+      const request2 = axios({
+        method: 'GET',
+        url: 'http://localhost:8080/api/getFighterStats/' + visitor
+      });
+      return request2;
+    }
+    axios.all([getHome(), getVisitor()])
+      .then(axios.spread((homeStats, visitorStats) => {
+        const data = fight;
+        data.homeFightStats = homeStats.data;
+        data.visitorFightStats = visitorStats.data;
+        dispatch({ type: 'FIGHTER_RESET' });
+        dispatch(setFight(data));
+      }));
   };
 }
+
+//check if fighter stat data is available.
+//if it's not 
+	//make get request to server. 
+	//server will make api calls to pull fighter info
+	//server will pass back fighter stat info. 
+//reducer will update fighter stats data.
+//reducer will pass back new state w/ fighter stat data. 
