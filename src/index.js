16b4ea294a1b5fdb78920a33b46c8cba77df2469
Christopher Lee <xchrislee@gmail.com>
2017-12-04 14:57:49 -0800

    Clean up entry point. Fix routing w/ RRv4

--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Switch } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory'
-import { ConnectedRouter, routerREducer, routerMiddleware, push, syncHistoryWithStore } from 'react-router-redux';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
 import configureStore from './stores/configureStore';
-import * as actions from './actions';
-import App from './components/app';
-import BettingPage from './components/BettingPage';
-import EventList from './components/BettingPage/Body/EventList';
-import FightCard from './components/BettingPage/Body/FightCard';
+import App from './components/App';
 
 const store = configureStore();
-// const history = syncHistoryWithStore(BrowserRouter, store);
 const history = createHistory();
-
-store.dispatch(actions.getEvents());
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <Switch>
-        <Route path="/" exact component={BettingPage} />
-      </Switch>
+      <App />
     </Router>
   </Provider>,
   document.getElementById('app'),
