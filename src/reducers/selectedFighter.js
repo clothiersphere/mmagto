d5d0af6666bd6934bb876ad750e6953183e342ec
Christopher Lee <xchrislee@gmail.com>
2017-12-04 15:20:14 -0800

    Lint

--- src/reducers/selectedFighter.js
+++ src/reducers/selectedFighter.js
@@ -14,6 +14,7 @@ export default function (state = initialState, action) {
       return setFighter(newState, action);
     case actionTypes.FIGHTER_RESET:
       return initialState;
+    default:
+      return state;
   }
-  return state;
 }
